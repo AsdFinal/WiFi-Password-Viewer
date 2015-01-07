5f5c3d34ff8ae67566e2bab0f9ad5615f6bd8ee1
ms519093175@gmail.com <ms519093175@gmail.com>
2015-01-07 14:20:26 +0800

    Initial commit

---
+++ app/src/main/java/asd/dogw/wifi_pw_viewer/MainActivity.java
@@ -0,0 +1,14 @@
+package asd.dogw.wifi_pw_viewer;
+
+import android.app.*;
+import android.os.*;
+
+public class MainActivity extends Activity 
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
